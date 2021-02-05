eeadad8bcc26d9d534a9d863e1129e978eb25e88
Sergio Barria <sergio.barria@icloud.com>
2021-02-05 00:10:47 -0500

    add products page, sort and filter functionality

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -7,7 +7,7 @@ const PageHero = ({ title, product }) => {
     <Wrapper>
       <div className='section-center'>
         <h3>
-          <Link to='/'>Home</Link>/
+          <Link to='/'>Home</Link>
           {product && <Link to='/products'>Products</Link>}/ {title}
         </h3>
       </div>
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -7,11 +7,117 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
+} from '../actions';
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  switch (action.type) {
+    case LOAD_PRODUCTS:
+      let maxPrice = action.payload.map(p => p.price);
+      maxPrice = Math.max(...maxPrice);
 
-export default filter_reducer
+      return {
+        ...state,
+        all_products: [...action.payload],
+        filtered_products: [...action.payload],
+        filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
+      };
+    case SET_GRIDVIEW:
+      return { ...state, grid_view: true };
+    case SET_LISTVIEW:
+      return { ...state, grid_view: false };
+    case UPDATE_SORT:
+      return { ...state, sort: action.payload };
+    case SORT_PRODUCTS:
+      const { sort, filtered_products } = state;
+      let tempProducts = [...filtered_products];
+      if (sort === 'price-lowest') {
+        tempProducts = tempProducts.sort((a, b) => {
+          if (a.price < b.price) {
+            return -1;
+          }
+          if (a.price > b.price) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+      if (sort === 'price-highest') {
+        tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+      }
+      if (sort === 'name-a') {
+        tempProducts = tempProducts.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        });
+      }
+      if (sort === 'name-z') {
+        tempProducts = tempProducts.sort((a, b) => {
+          return b.name.localeCompare(a.name);
+        });
+      }
+      return { ...state, filtered_products: tempProducts };
+    case UPDATE_FILTERS:
+      const { name, value } = action.payload;
+      return { ...state, filters: { ...state.filters, [name]: value } };
+    case FILTER_PRODUCTS:
+      const { all_products } = state;
+      const { text, category, company, color, price, shipping } = state.filters;
+
+      let filterProducts = [...all_products];
+      // filtering
+      // text
+      if (text) {
+        filterProducts = filterProducts.filter(product => {
+          return product.name.toLowerCase().startsWith(text);
+        });
+      }
+
+      // category
+      if (category !== 'all') {
+        filterProducts = filterProducts.filter(
+          product => product.category === category
+        );
+      }
+
+      // company
+      if (company !== 'all') {
+        filterProducts = filterProducts.filter(
+          product => product.company === company
+        );
+      }
+
+      // colors
+      if (color !== 'all') {
+        filterProducts = filterProducts.filter(product => {
+          return product.colors.find(c => c === color);
+        });
+      }
+
+      // price
+      filterProducts = filterProducts.filter(product => product.price <= price);
+
+      // shipping
+      if (shipping) {
+        filterProducts = filterProducts.filter(
+          product => product.shipping === true
+        );
+      }
+
+      return { ...state, filtered_products: filterProducts };
+    case CLEAR_FILTERS:
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          text: '',
+          company: 'all',
+          category: 'all',
+          color: 'all',
+          price: state.filters.max_price,
+          shipping: false,
+        },
+      };
+  }
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default filter_reducer;
