52dc0efc08fac20d1d4a973894139018f8157e9a
Sergio Barria <sergio.barria@icloud.com>
2021-02-05 17:05:13 -0500

    added authentication with auth0

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -9,6 +9,8 @@ import { useUserContext } from '../context/user_context';
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext();
   const { total_items } = useCartContext();
+  const { loginWithRedirect, myUser, logout } = useUserContext();
+
   return (
     <Wrapper className='cart-btn-wrapper'>
       <Link to='/cart' className='cart-btn' onClick={closeSidebar}>
@@ -18,9 +20,19 @@ const CartButtons = () => {
           <span className='cart-value'>{total_items}</span>
         </span>
       </Link>
-      <button className='auth-btn' type='button'>
-        Login <FaUserPlus />
-      </button>
+      {myUser ? (
+        <button
+          type='button'
+          className='auth-btn'
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
+          Logout <FaUserMinus />
+        </button>
+      ) : (
+        <button className='auth-btn' type='button' onClick={loginWithRedirect}>
+          Login <FaUserPlus />
+        </button>
+      )}
     </Wrapper>
   );
 };
